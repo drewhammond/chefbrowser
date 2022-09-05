5dce314d7d092a39e43ca069098e9d9ce0329495
Drew Hammond <2405390+drewhammond@users.noreply.github.com>
2022-09-05 17:40:47 -0400

    add databags endpoints

---
+++ internal/chef/databags.go
@@ -0,0 +1,36 @@
+package chef
+
+import (
+	"context"
+	"github.com/go-chef/chef"
+)
+
+func (s Service) GetDatabags(ctx context.Context) (interface{}, error) {
+	environments, err := s.client.DataBags.List()
+	if err != nil {
+		s.log.Error("failed to list databags")
+		return nil, err
+	}
+
+	return environments, nil
+}
+
+func (s Service) GetDatabagItems(ctx context.Context, name string) (*chef.DataBagListResult, error) {
+	items, err := s.client.DataBags.ListItems(name)
+	if err != nil {
+		s.log.Error("failed to list items in databag")
+		return items, err
+	}
+
+	return items, nil
+}
+
+func (s Service) GetDatabagItemContent(ctx context.Context, databag string, item string) (chef.DataBagItem, error) {
+	contents, err := s.client.DataBags.GetItem(databag, item)
+	if err != nil {
+		s.log.Error("failed to list items in databag")
+		return contents, err
+	}
+
+	return contents, nil
+}
