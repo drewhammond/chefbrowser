6197d8d79a3ae36c01b79659e906865a16a37952
Drew Hammond <2405390+drewhammond@users.noreply.github.com>
2022-10-19 00:03:30 -0400

    function to merge node attributes

--- internal/chef/nodes.go
+++ internal/chef/nodes.go
@@ -3,10 +3,12 @@ package chef
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"sort"
 
 	"github.com/drewhammond/chefbrowser/internal/util"
 	"github.com/go-chef/chef"
+	"github.com/imdario/mergo"
 )
 
 type NodeList struct {
@@ -15,7 +17,10 @@ type NodeList struct {
 
 type Node struct {
 	chef.Node
+	MergedAttributes map[string]interface{}
 }
+
+var ErrPathNotFound = errors.New("attribute not found at path")
 
 func (s Service) GetNodes(ctx context.Context) (*NodeList, error) {
 	nodes, err := s.client.Nodes.List()
@@ -62,10 +67,41 @@ func (s Service) GetNode(ctx context.Context, name string) (*Node, error) {
 		return nil, err
 	}
 
-	node.AutomaticAttributes = util.MakeJSONPath(node.AutomaticAttributes, "$")
-	node.NormalAttributes = util.MakeJSONPath(node.NormalAttributes, "$")
-	node.DefaultAttributes = util.MakeJSONPath(node.DefaultAttributes, "$")
-	node.OverrideAttributes = util.MakeJSONPath(node.OverrideAttributes, "$")
+	ret := &Node{Node: node}
+	ret.AutomaticAttributes = util.MakeJSONPath(node.AutomaticAttributes, "$")
+	ret.NormalAttributes = util.MakeJSONPath(node.NormalAttributes, "$")
+	ret.DefaultAttributes = util.MakeJSONPath(node.DefaultAttributes, "$")
+	ret.OverrideAttributes = util.MakeJSONPath(node.OverrideAttributes, "$")
+	ret.MergedAttributes = util.MakeJSONPath(ret.MergeAttributes(), "$")
 
-	return &Node{node}, nil
+	return ret, nil
+}
+
+// MergeAttributes returns the merged set of all node attributes taking attribute precedence into consideration.
+// Ref: https://docs.chef.io/attribute_precedence/
+func (s Node) MergeAttributes() map[string]interface{} {
+	attrs := s.DefaultAttributes
+	_ = mergo.Merge(&attrs, s.NormalAttributes, mergo.WithOverride)
+	_ = mergo.Merge(&attrs, s.OverrideAttributes, mergo.WithOverride)
+	_ = mergo.Merge(&attrs, s.AutomaticAttributes, mergo.WithOverride)
+	return attrs
+}
+
+// GetEffectiveAttributeValue returns the effective attribute value of a given path considering attribute precedence.
+func (s Node) GetEffectiveAttributeValue(paths ...string) (interface{}, error) {
+	return lookupAttribute(s.MergedAttributes, paths...)
+}
+
+// lookupAttribute is a function from go-chef, but we use it differently here since all attributes
+// are merged instead of just a single one when requested
+func lookupAttribute(attrs map[string]interface{}, paths ...string) (interface{}, error) {
+	currentPath, remainingPaths := paths[0], paths[1:]
+	if attr, ok := attrs[currentPath]; ok {
+		if len(remainingPaths) <= 0 {
+			return attr, nil
+		}
+		return lookupAttribute(attr.(map[string]interface{}), remainingPaths...)
+	}
+
+	return nil, ErrPathNotFound
 }
